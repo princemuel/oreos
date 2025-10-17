181c9d406bd761b7044172bfe5a349941d93660a
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-10-17 23:27:01 +0100

    feat(kernel): scaffold free-standing rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! { loop {} }
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! { loop {} }
