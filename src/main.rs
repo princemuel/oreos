c47267c2886a20629e60a6a8b5c1c1271b9c8863
Prince Muel <vansomecsam@gmail.com>
2026-06-18 10:44:03 +0100

    build: improve build config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
@@ -8,7 +8,7 @@ use oreos::println;
 /// This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo<'_>) -> ! {
-    println!("{}", info);
+    println!("{info}",);
     loop {}
 }
 
